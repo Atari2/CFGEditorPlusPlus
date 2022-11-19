9109ef1512c893e635b755ebab5ca8a1c2a83a59
Alessio Rosiello <alex.robassomero@gmail.com>
2022-11-19 13:39:18 +0100

    Remove unused QT_QMAKE_EXECUTABLE variable and remove exit at end.

--- build_script.bat
+++ build_script.bat
@@ -1,6 +1,5 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cmake.exe -S . -B build -GNinja "-DCMAKE_BUILD_TYPE:String=Release" "-DQT_QMAKE_EXECUTABLE:STRING=C:/Qt/6.4.0/msvc2019_64/bin/qmake.exe" "-DCMAKE_PREFIX_PATH:STRING=C:/Qt/6.4.0/msvc2019_64" "-DCMAKE_C_COMPILER:STRING=cl.exe" "-DCMAKE_CXX_COMPILER:STRING=cl.exe"
+cmake.exe -S . -B build -GNinja "-DCMAKE_BUILD_TYPE:String=Release" "-DCMAKE_PREFIX_PATH:STRING=C:/Qt/6.4.0/msvc2019_64" "-DCMAKE_C_COMPILER:STRING=cl.exe" "-DCMAKE_CXX_COMPILER:STRING=cl.exe"
 cd build
 cmake.exe --build . --target all
 cd ..
-exit
